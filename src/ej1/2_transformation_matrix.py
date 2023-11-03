0536d59430876d17b923a8ab84d6c1b38acf8e7d
dlimachi <dlimachi@itba.edu.ar>
2023-11-03 05:11:50 -0300

    Modified comments

--- src/ej1/2_transformation_matrix.py
+++ src/ej1/2_transformation_matrix.py
@@ -3,13 +3,15 @@ import numpy as np
 
 w = 9.75
 h = 6.5
-# Puntos de calibración (coordenadas en metros y coordenadas en píxeles)
+# Calibration points (coordinates in meters and coordinates in pixels)
 world_points = np.array([
     [-w, -h], 
     [-w, h],
     [w, -h],
     [w, h],
 ], dtype=np.float32)
+
+# Array of target points
 image_points = np.array([
     # Bottom Left
     [226, 610],
@@ -21,12 +23,10 @@ image_points = np.array([
     [922, 158],
 ], dtype=np.float32)
 
-# Calcular la matriz de proyección perspectiva
+# Calculate the perspective projection matrix
 matrix = cv2.getPerspectiveTransform(world_points, image_points)
 print(matrix.tolist())
 
-# Transformar las coordenadas de un punto en metros a coordenadas en píxeles
+# Transform the coordinates of a point in meters to coordinates in pixels
 # point_in_meters = np.array([[]], dtype=np.float32)
 # transformed_point = cv2.perspectiveTransform(point_in_meters, matrix)
-
-# transformed_point ahora contiene las coordenadas en píxeles del punto en la imagen
